e61297a79db67d148cef7e3e44ad119462b7c4d0
JuliaNortman <43141960+JuliaNortman@users.noreply.github.com>
2020-01-13 19:52:27 +0200

    Add validation to the DesignTaco and Order

--- tacocloud/src/main/java/sia/tacocloud/DesignTacoController.java
+++ tacocloud/src/main/java/sia/tacocloud/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
@@ -37,7 +38,23 @@ public class DesignTacoController {
 	}
 
 	@PostMapping
-	public String processDesign(Taco design) {
+	public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+		if (errors.hasErrors()) {
+			List<Ingredient> ingredients = Arrays.asList(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+					new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+					new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+					new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+					new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+					new Ingredient("LETC", "Lettuce", Type.VEGGIES), new Ingredient("CHED", "Cheddar", Type.CHEESE),
+					new Ingredient("JACK", "Monterrey Jack", Type.CHEESE), new Ingredient("SLSA", "Salsa", Type.SAUCE),
+					new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+			Type[] types = Ingredient.Type.values();
+			for (Type type : types) {
+				model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+			}
+			return "design";
+		}
+
 		// Save the taco design...
 		// We'll do this in chapter 3
 		log.info("Processing design: " + design);
--- tacocloud/src/main/java/sia/tacocloud/OrderController.java
+++ tacocloud/src/main/java/sia/tacocloud/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +17,15 @@ public class OrderController {
 	@GetMapping("/current")
 	public String orderForm(Model model) {
 		model.addAttribute("order", new Order());
-		return "orderForm";
+		return "/orderForm";
 	}
 	
 	@PostMapping
-	public String processOrder(Order order) {
+	public String processOrder(@Valid @ModelAttribute("order") Order order, Errors errors, Model model) {
+		if(errors.hasErrors()) {
+			return "orderForm";
+		}
+		
 		log.info("Order submitted: " + order);
 		return "redirect:/";
 	}
