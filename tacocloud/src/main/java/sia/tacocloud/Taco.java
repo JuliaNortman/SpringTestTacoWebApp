a220a661d282616d3826172bb69a35189092c7c3
JuliaNortman <43141960+JuliaNortman@users.noreply.github.com>
2020-01-13 20:21:42 +0200

    Add styles for errors

--- tacocloud/src/main/java/sia/tacocloud/Taco.java
+++ tacocloud/src/main/java/sia/tacocloud/Taco.java
@@ -13,6 +13,7 @@ public class Taco {
 	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
 	
+	@NotNull(message="You must choose at least 1 ingredient")
 	@Size(min=1, message="You must choose at least 1 ingredient")
 	private List<String> ingredients;
 }
